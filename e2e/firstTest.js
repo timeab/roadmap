b75524fefcadaefffca4ca71074b529a90e94068
timeab <timeab@pentalog.com>
2019-01-11 14:55:51 +0200

    Created e2e tests

---
+++ e2e/firstTest.js
@@ -0,0 +1,48 @@
+
+import commonSelectors from './selectors/commonSelectors';
+import descriptionContent from './selectors/descriptionContent';
+
+const commonSelector = new commonSelectors;
+const categoryDescriptionItems = { Kids: 1, Home: 5, Auto: 5, Food: 1 };
+
+fixture`Testing product app`// declare the fixture
+    .page`http://localhost:3000/`;  // specify the start page
+
+
+//then create a test and place your code there
+test('Check description from the first selected option', async t => {
+    await t
+        .click(commonSelector.getAllCheckBoxes().nth(0))
+        .expect(Boolean(commonSelector.getAllCheckBoxes().nth(0).child().value)).eql(true)
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(commonSelector.getText().textContent).eql(descriptionContent.description_kids);
+});
+test('Check addToCart functionality', async t => {
+    await t
+        .setNativeDialogHandler(() => true)
+        .click(commonSelector.getButton())
+        .click(commonSelector.getAllCheckBoxes().nth(0))
+        .expect(Boolean(commonSelector.getAllCheckBoxes().nth(0).child().value)).eql(true);
+});
+test('Check all checkbox items', async t => {
+    Object.keys(categoryDescriptionItems).map(async option => {
+        await t.click(commonSelector.getAllCheckBoxes().withText(option))
+            .expect(Boolean(commonSelector.getAllCheckBoxes().nth(0).child().value)).eql(true);
+    });
+});
+test('Uncheck all checkbox items', async t => {
+    Object.keys(categoryDescriptionItems).map(async option => {
+        await t.click(commonSelector.getAllCheckBoxes().withText(option))
+            .expect(Boolean(commonSelector.getAllCheckBoxes().nth(0).child().value)).eql(false);
+    });
+});
+test('Check if selecting a category contains the right number of description items', async t => {
+    await Promise.all(Object.entries(categoryDescriptionItems).map(async (items, index) => {
+        const [option, descriptionLength] = [...items];
+        const cardListItems = commonSelector.getAllCardList().child();
+        const checkBoxItem = commonSelector.getAllCheckBoxes().nth(index).withText(option);
+        await t.click(checkBoxItem)
+            .expect(cardListItems.find('.description').count).eql(descriptionLength)
+            .click(commonSelector.getAllCheckBoxes().withText(option))
+    }));
+});
---
+++ e2e/selectors/descriptionContent.js
@@ -0,0 +1,10 @@
+const description = {
+    description_kids: ' Description:A teddy bear is a soft toy in the form of a bear. Developed apparently simultaneously by toymakers Morris Michtom in the U.S. and Richard Steiff in Germany in the early years of the 20th century',
+    description_chair: ' Description:Its primary features are two pieces of a durable material, attached as back and seat to one another at a 90° or slightly greater angle, with usually the four corners of the horizontal seat attached in turn to four legs.',
+    description_lamp: ' Description:A lava lamp (or Astro lamp) is a decorative novelty item, invented in 1963 by British accountant Edward Craven Walker, the founder of the British lighting company Mathmos',
+    description_sofa: ' Description:Complete your living room with a stylish sofa finished in premium leather or luxury fabric, for exceptional comfort. Shop the collection at Furniture Village.',
+    description_chandelier: ' Description:A chandelier is a branched ornamental light fixture designed to be mounted on ceilings or walls. Chandeliers are often ornate, and normally use incandescent light bulbs, though some modern designs also use fluorescent lamps and recently LEDs',
+    description_table: ' Description:A table is an item of furniture with a flat top and one or more legs, used as a surface for working at, eating from or on which to place things.'
+};
+
+export default description;
